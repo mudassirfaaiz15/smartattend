ff6506f36337357be5fbf183ae7c6d7de59c5614
Mudassir Faaiz <23A91A1239@aec.edu.in>
2026-01-20 09:58:11 +0530

    Fix build issues and stabilize frontend

--- client/src/components/QRScanner.jsx
+++ client/src/components/QRScanner.jsx
@@ -1,16 +1,7 @@
-import { Html5QrcodeScanner } from "html5-qrcode";
-import { useEffect } from "react";
-
-
-export default function QRScanner({ onScan }) {
-useEffect(() => {
-const scanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
-scanner.render(result => {
-onScan(result);
-scanner.clear();
-});
-}, []);
-
-
-return <div id="reader" />;
+export default function QRScanner() {
+  return (
+    <div>
+      <h2>QR Scanner temporarily disabled</h2>
+    </div>
+  );
 }
